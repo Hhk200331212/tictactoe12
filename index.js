73e05365c402f262e1cbe401fe5613b440470cc8
hhk2003 <humayunhassan1212@gmail.com>
2021-07-31 17:30:06 +0500

    bot

---
+++ index.js
@@ -0,0 +1,4 @@
+const TicTacToe = require('discord-tictactoe');
+new TicTacToe({ language: 'en', command: '!ttt' })
+  .login('process.env.token')
+  .then(() => console.log('TicTacToe bot is ready to be used.'));
